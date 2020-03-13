ede7fef6e775659fd1ea3ec6bd7cce910d1e687f
‘penjoe’ <pennockjoe@gmail.com>
2020-03-12 17:09:15 -0700

    added framework for two functions that one each adds a header and a footer to the table

--- script/app.js
+++ script/app.js
@@ -2,10 +2,12 @@
 
 //Array that will hold all store locations for easier reference
 var storeList = [];
+
 //Number of hours each store is open
 var hoursOpen = 14;
+
 //Array of strings for each hour the store is open that will be used to write onto the table header cells 
-var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+var hoursofOperation = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
 
 //This constructor function will create a template from which I can make any number of store locations by creating an instance for each desired store
 
@@ -40,6 +42,7 @@ NewStore.prototype.salesData = function() {
   var cookieSum = 0;
 
   for (var i = 0; i < hoursOpen; i++) {
+    
     //customers served per hour
     var customersPerHour = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
     hourlyCustomers.push(customersPerHour);
@@ -73,10 +76,12 @@ NewStore.prototype.createTable = function() {
 
   //This loops will create 14 cells per row
   for (var cookiesSold = 0 ;  cookiesSold < this.cookiesPerHour.length ; cookiesSold++) {
+
     cellEl = document.createElement('td');
     cellEl.textContent = this.cookiesPerHour[cookiesSold];
     rowEl.appendChild(cellEl);
   }
+
   //Adds daily totals to end column of table
   cellEl = document.createElement('td');
   cellEl.textContent = this.totalCookiesSold;
@@ -84,6 +89,39 @@ NewStore.prototype.createTable = function() {
 
 }
 
+//Adds a header to the table with an hour for each column. Hours are stored in a global array.
+function createTableHeader() {
+  var header = document.getElementById('sales-table');
+  var headerRow = document.createElement('tr');
+  var headerCell = document.createElement('td');
+  headerCell.textContent = 'Hours Of Operation';
+
+  for (var i = 0 ; i < (hoursOfOperation.length +2) ; i++) {
+
+  }
+
+}
+
+//Adds a footer to the table with hourly totals and tracks a brand total cookies sold.
+function createTableFooter() {
+  var dailySum = 0;
+  var hourlySum = 0;
+  var footer = document.getElementById('sales-table');
+  var footerRow = document.createElement('tr');
+  var footerCell = document.createElement('td');
+  cell.textContent = 'Hourly Sales Total';
+  footer.appendChild(footerRow);
+  footerRow.appendChild(footerCell)
+
+  for (var i = 0; i < hoursOpen; i++) {
+
+
+    for (var j = 0; j < storeList.length; j++) {
+
+    }
+  }
+
+}
 //These are the instances for each individual store created from the constructor function using the 'new' keyword
 var seattleStore = new NewStore('Seattle', 23, 65, 6.3);
 var tokyoStore = new NewStore('Tokyo', 3, 24, 1.2);
