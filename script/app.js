2dcb4ed5f8ab8f7c0d2122e03a2981aeee249b84
‘penjoe’ <pennockjoe@gmail.com>
2020-03-14 21:47:21 -0700

    got full table functionality to work with form inputs

--- script/app.js
+++ script/app.js
@@ -1,25 +1,25 @@
 'use strict'
 
-//Array that will hold all store locations for easier reference
+// Array that will hold all store locations for easier reference
 var storeList = [];
 
-//Number of hours each store is open
+// Number of hours each store is open
 var hoursOpen = 14;
 
-//Array of strings for each hour the store is open that will be used to write onto the table header cells 
+// Array of strings for each hour the store is open that will be used to write onto the table header cells 
 var hoursOfOperation = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
 
-//This constructor function will create a template from which I can make any number of store locations by creating an instance for each desired store
+// This constructor function will create a template from which I can make any number of store locations by creating an instance for each desired store
 
 function NewStore(storeName, minCustomer, maxCustomer, avgCookies) {
 
-  //Properties for objects to be created, defined when creating object instance
+  // Properties for objects to be created, defined when creating object instance
   this.storeName = storeName;
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
   this.avgCookies = avgCookies;
   
-  //This is all of my store sales metrics stored in an object
+  // This is all of my store sales metrics stored in an object
   var hourlyData = this.salesData();
 
   this.customersPerHour = hourlyData.customerData;        //customers per hour
@@ -27,12 +27,14 @@ function NewStore(storeName, minCustomer, maxCustomer, avgCookies) {
   this.cookiesPerHour = hourlyData.cookieData;            //cookies per hour
   this.totalCookiesSold = hourlyData.cookieTotals;        //cookie sum
 
-  //Pushes each store to a global array for easy reference
+  // Pushes each store to a global array for easy reference
   storeList.push(this);
-  this.createTable();
+
+  // this.createTable();
 }
 
-//Calculates hourly and total customers and hourly and total sales and store them in an array
+
+// Calculates hourly and total customers and hourly and total sales and store them in an array
 NewStore.prototype.salesData = function() {
 
   // var totals = this.randomCustomer();
@@ -43,15 +45,15 @@ NewStore.prototype.salesData = function() {
 
   for (var i = 0; i < hoursOpen; i++) {
     
-    //customers served per hour
+    // customers served per hour
     var customersPerHour = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
     hourlyCustomers.push(customersPerHour);
-    //cookies sold per hour
+    // cookies sold per hour
     var cookiesPerHour = Math.floor(customersPerHour * this.avgCookies);
     hourlyCookies.push(cookiesPerHour);
-    //total customers
+    // total customers
     customerSum = customerSum + customersPerHour;
-    //total cookies
+    // total cookies
     cookieSum = cookieSum + cookiesPerHour;
     
   }
@@ -63,10 +65,10 @@ NewStore.prototype.salesData = function() {
   }
 }
 
-//This method will create a table and populate it with the sales data for each store.
+// This method will create a table and populate it with the sales data for each store.
 NewStore.prototype.createTable = function() {
   
-  //Variable storing the table elements
+  // Variable storing the table elements
   var tableEl = document.getElementById('sales-table');
   var rowEl = document.createElement('tr');
   var cellEl = document.createElement('td');
@@ -74,7 +76,7 @@ NewStore.prototype.createTable = function() {
   tableEl.appendChild(rowEl);
   rowEl.appendChild(cellEl);
 
-  //This loops will create 14 cells per row
+  // This loops will create 14 cells per row
   for (var cookiesSold = 0 ;  cookiesSold < this.cookiesPerHour.length ; cookiesSold++) {
 
     cellEl = document.createElement('td');
@@ -82,14 +84,13 @@ NewStore.prototype.createTable = function() {
     rowEl.appendChild(cellEl);
   }
 
-  //Adds daily totals to end column of table
+  // Adds daily totals to end column of table
   cellEl = document.createElement('td');
   cellEl.textContent = this.totalCookiesSold;
   rowEl.appendChild(cellEl);
-
 }
 
-//Adds a header to the table with an hour for each column. Hours are stored in a global array.
+// Adds a header to the table with an hour for each column. Hours are stored in a global array.
 function createTableHeader() {
   var header = document.getElementById('sales-table');
   var headerRow = document.createElement('tr');
@@ -97,7 +98,7 @@ function createTableHeader() {
   
   headerRow.appendChild(headerCell);
 
-  //Loops through array with each hour open as a string and adds that string to a cell
+  // Loops through array with each hour open as a string and adds that string to a cell
   for (var i = 0 ; i <= (hoursOfOperation.length) ; i++) {
 
     headerCell = document.createElement('td');
@@ -112,43 +113,91 @@ function createTableHeader() {
 
 }
 
-//Adds a footer to the table with hourly totals and tracks a brand total cookies sold.
+var seattleStore = new NewStore('Seattle', 23, 65, 6.3);
+var tokyoStore = new NewStore('Tokyo', 3, 24, 1.2);
+var dubaiStore = new NewStore('Dubai', 11, 38, 3.7);
+var parisStore = new NewStore('Paris', 20, 38, 2.3);
+var limaStore = new NewStore('Lima', 2, 16, 4.6);
+
+// Adds a footer to the table with hourly totals and tracks a brand total cookies sold.
 function createTableFooter() {
   var dailySum = 0;
   var footer = document.getElementById('sales-table');
   var footerRow = document.createElement('tr');
   var footerCell = document.createElement('td');
   footerCell.textContent = 'Hourly Sales Total';
-  // footer.appendChild(footerRow);
-  footerRow.appendChild(footerCell)
+  footerRow.appendChild(footerCell);
 
+  // Outer loop goes through hours totals
   for (var i = 0; i < hoursOpen; i++) {
 
     footerCell = document.createElement('td');
     var hourlySum = 0;
+
+    // Inner loops goes through the store list and grabs the sales from each store
     for (var j = 0; j < storeList.length; j++) {
 
       hourlySum = hourlySum + storeList[j].cookiesPerHour[i];
 
     }
 
+    // adds a cell each time the outer loop runs that appends daily sum to the cell
     footerCell.textContent = hourlySum;
     footerRow.appendChild(footerCell)
     dailySum = dailySum + hourlySum;
 
   }
-
+  
+  // After loops end, creates a cell and appends brand total to end cell of footer
   footerCell = document.createElement('td');
   footerCell.textContent = ('Company Total: ' + dailySum);
   footerRow.appendChild(footerCell);
   footer.appendChild(footerRow);
   
 }
-//These are the instances for each individual store created from the constructor function using the 'new' keyword
+
+// variable used to reference table html element
+var addNewStoreRow = document.getElementById('add-store');
+// This function takes user data input into the form and uses that to create a new store on the table
+function addToForm(event) {
+
+  //cancels default behavior of event
+  event.preventDefault();
+  
+  // variables used to reference the specific html elements and their values
+  var storeName = event.target.storeName.value;
+  var minimumCustomers = event.target.minCust.value;
+  var maximumCustomers = event.target.maxCust.value;
+  var AverageSales = event.target.avgCookies.value;
+
+  // User input from form is used to make a new store object via constructor
+  new NewStore(storeName, minimumCustomers, maximumCustomers, AverageSales)
+  
+}
+
+addNewStoreRow.addEventListener('submit' , addToForm);
+
+// variable used to reference html button element
+var formSubmitButton = document.getElementById('submit-button');
+// function that will render new row to the table upon click
+function renderNewStore() {
+  
+  document.getElementById('sales-table').innerHTML = null;
+  createTableHeader();
+  
+  for (var i = 0; i < storeList.length; i++) {
+    storeList[i].createTable();
+  }
+  createTableFooter();
+}
+
+formSubmitButton.addEventListener('click' , renderNewStore);
+
+// These are the instances for each individual store created from the constructor function using the 'new' keyword
 createTableHeader();
-var seattleStore = new NewStore('Seattle', 23, 65, 6.3);
-var tokyoStore = new NewStore('Tokyo', 3, 24, 1.2);
-var dubaiStore = new NewStore('Dubai', 11, 38, 3.7);
-var parisStore = new NewStore('Paris', 20, 38, 2.3);
-var limaStore = new NewStore('Lima', 2, 16, 4.6);
+seattleStore.createTable();
+tokyoStore.createTable();
+dubaiStore.createTable();
+parisStore.createTable();
+limaStore.createTable();
 createTableFooter();
